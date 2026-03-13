e357f27cdddf8860dc965968787af68dcbc35125
Bonnie Ji <bbji@andrew.cmu.edu>
2026-03-13 13:47:30 -0400

    Initial Commit

---
+++ Umbrella App/Umbrella_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Umbrella_AppApp.swift
+//  Umbrella App
+//
+//  Created by Bonnie Ji on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Umbrella_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Umbrella AppTests/Umbrella_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Umbrella_AppTests.swift
+//  Umbrella AppTests
+//
+//  Created by Bonnie Ji on 3/13/26.
+//
+
+import Testing
+
+struct Umbrella_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
